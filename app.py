6b9f9e72086f8aed8542e159ccd13599d735e95b
Lucas Wall <lucaswallbruno@gmail.com>
2025-07-10 15:47:28 -0300

    Feat: Prepare app for Streamlit hosting

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import warnings
