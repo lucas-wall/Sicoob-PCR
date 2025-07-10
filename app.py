823498b450488bc3c77c42e098ade8b33e1dfc37
Lucas Wall <lucaswallbruno@gmail.com>
2025-07-10 16:25:13 -0300

    Add runtime installation for markdown and reportlab

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "markdown", "reportlab"])
 import streamlit as st
 import pandas as pd
 import numpy as np
